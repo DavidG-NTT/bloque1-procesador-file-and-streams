0812d41b9c6e2b78bdc5dd5e175007ef96479ef8
dgoncord <dgoncord@emeal.nttdata.com>
2026-02-25 16:19:42 +0100

    Add initial project files including .gitignore, Person class, and main application logic

---
+++ src/InvalidLineFormatException.java
@@ -0,0 +1,6 @@
+public class InvalidLineFormatException extends Exception {
+    // Constructor con mensaje de error
+    public InvalidLineFormatException(String message) {
+        super(message);
+    }
+}
