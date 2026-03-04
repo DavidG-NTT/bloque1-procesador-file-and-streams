bcc4e11af9b7fd7b81ade5f6ffb9e23350211016
dgoncord <dgoncord@emeal.nttdata.com>
2026-03-04 11:46:42 +0100

    Se mejorado un poco separando el método que había en el main de filtros a una clase nueva y se ha creado un método de formatoImpresion.

--- src/main/java/com/ejercicio1/Main.java
+++ src/main/java/com/ejercicio1/Main.java
@@ -1,11 +1,13 @@
 package com.ejercicio1;
 
 import com.ejercicio1.application.Data;
+import com.ejercicio1.application.FilteringStream;
 import com.ejercicio1.infrastructure.ProcessFile;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("\nLECTURA DEL FICHERO CSV");
         validateArgs(args);
         Data datos = new Data();
         ProcessFile.procesarArchivo(args[0], datos);
@@ -15,7 +17,9 @@ public class Main {
         System.out.println("Total de personas cargadas correctamente: " + datos.getPersonas().size());
         System.out.println("Total de líneas incorrectas: " + datos.getLineasIncorrectas());
 
-        filtrosStreams(datos);
+        // Filtrar y mostrar resultados usando
+        System.out.println("\n\nFILTROS CON STREAMS");
+        FilteringStream.filtrosStreams(datos);
     }
 
     public static void validateArgs(String[] args) {
@@ -23,49 +27,5 @@ public class Main {
             System.out.println("No se han proporcionado argumentos.");
             System.exit(1);
         }
-    }
-
-    public static void filtrosStreams(Data datos) {
-        System.out.println("\n--- Personas menores de 25 años ---");
-         datos.getPersonas().stream()
-                .filter(p -> p.getAge() < 25 && p.getAge() != 0)
-                .forEach(p ->
-                        System.out.println("Name: " + p.getName() +
-                                ". Town: " + (p.getTown().trim().isEmpty() ? "unknown" : p.getTown().trim()) +
-                                ". Age: " + p.getAge())
-                );
-
-        System.out.println("\n--- Eliminar personas cuyo nombre empieza por “A” ---");
-        datos.getPersonas().stream()
-                .filter(p -> !p.getName().startsWith("A"))
-                .forEach(p ->
-                        System.out.println("Name: " + p.getName() +
-                                ". Town: " + (p.getTown().trim().isEmpty() ? "unknown" : p.getTown().trim()) +
-                                ". Age: " + (p.getAge() == 0 ? "unknown" : p.getAge()))
-                );
-
-        System.out.println("\n --- Buscar la primera persona con ciudad “Madrid” ---");
-        datos.getPersonas().stream()
-                .filter(p -> p.getAge() < 25 && p.getAge() != 0)
-                .filter(p -> p.getTown().equalsIgnoreCase("Madrid"))
-                .findFirst()
-                .ifPresentOrElse(p ->
-                        System.out.println("Name: " + p.getName() +
-                                ". Town: " + (p.getTown().trim().isEmpty() ? "unknown" : p.getTown().trim()) +
-                                ". Age: " + p.getAge()),
-                        () -> System.out.println("No se encontró ninguna persona menor de 25 años en Madrid")
-                );
-
-        System.out.println("\n --- Buscar la primera persona con ciudad “Barcelona” ---");
-        datos.getPersonas().stream()
-                .filter(p -> p.getAge() < 25 && p.getAge() != 0)
-                .filter(p -> p.getTown().equalsIgnoreCase("Barcelona"))
-                .findFirst()
-                .ifPresentOrElse(p ->
-                                System.out.println("Name: " + p.getName() +
-                                        ". Town: " + (p.getTown().trim().isEmpty() ? "unknown" : p.getTown().trim()) +
-                                        ". Age: " + p.getAge()),
-                        () -> System.out.println("No se encontró ninguna persona menor de 25 años en Barcelona")
-                );
     }
 }
